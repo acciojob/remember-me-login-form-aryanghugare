51e1384b7396585f8cdeda1e0eda1fbf13f348cb
Aryan Ghugare <aryanghugare75@gmail.com>
2026-02-12 15:12:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const submitBtn = document.querySelector("#submit");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const rememberMe = document.querySelector("#checkbox");
+const existingBtn = document.querySelector("#existing");
+
+
+let credentails = JSON.parse(localStorage.get("data")) || {};
+
+if(credentails.password){
+	existingBtn.style.display = "block";
+}
+
+submitBtn.addEventListener("click",(e)={
+	e.preventDefault();
+	
+	let usernameValue = username.value ;
+	let passwordValue = password.value ;
+	alert(`Logged in as ${usernameValue}`);
+	let remember = rememberMe.value ;
+	credentails.username = usernameValue ;
+	credentails.password = passwordValue ;
+	if(remember){
+		localStorage.setItem("data",JSON.stringify(credentails));
+	}
+	else {
+         localStorage.clear();
+	}
+	
+})
+
+
+
+
